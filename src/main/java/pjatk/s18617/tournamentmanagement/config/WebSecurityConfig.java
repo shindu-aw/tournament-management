8095c47941578404149f5ed8855acc6fe1d3f908
shindu-aw <adamwoy1999@gmail.com>
2025-07-02 01:01:47 +0200

    feat: add registration

--- src/main/java/pjatk/s18617/tournamentmanagement/config/WebSecurityConfig.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/config/WebSecurityConfig.java
@@ -2,10 +2,13 @@ package pjatk.s18617.tournamentmanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,8 +30,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                //.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/home", "/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
@@ -49,6 +53,18 @@ public class WebSecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
--- src/main/java/pjatk/s18617/tournamentmanagement/controllers/AuthController.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/controllers/AuthController.java
@@ -1,10 +1,23 @@
 package pjatk.s18617.tournamentmanagement.controllers;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pjatk.s18617.tournamentmanagement.dtos.UserRegistrationDto;
+import pjatk.s18617.tournamentmanagement.model.User;
+import pjatk.s18617.tournamentmanagement.services.UserService;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
 
     @GetMapping(value = {"/", "/home"})
     public String home() {
@@ -19,6 +32,21 @@ public class AuthController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("userRegistrationDto", new UserRegistrationDto());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid UserRegistrationDto userRegistrationDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        userService.register(userRegistrationDto);
+        return "redirect:/login";
     }
 
 }
