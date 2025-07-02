815d0e93e1d8e2ef96ee9f700cae7ad353d60739
shindu-aw <adamwoy1999@gmail.com>
2025-07-02 11:16:30 +0200

    feat: add NotFoundException

---
+++ src/main/java/pjatk/s18617/tournamentmanagement/controllers/NotFoundException.java
@@ -0,0 +1,26 @@
+package pjatk.s18617.tournamentmanagement.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Nie znaleziono")
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
