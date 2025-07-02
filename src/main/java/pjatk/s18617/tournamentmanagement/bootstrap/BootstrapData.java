9bbd236dba4254a97aea1ff6703800bae314cc0d
shindu-aw <adamwoy1999@gmail.com>
2025-07-02 02:07:58 +0200

    feat: add games, locations, and tournaments to bootstrap data

--- src/main/java/pjatk/s18617/tournamentmanagement/bootstrap/BootstrapData.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/bootstrap/BootstrapData.java
@@ -5,9 +5,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import pjatk.s18617.tournamentmanagement.model.Game;
+import pjatk.s18617.tournamentmanagement.model.Location;
+import pjatk.s18617.tournamentmanagement.model.Tournament;
 import pjatk.s18617.tournamentmanagement.model.User;
+import pjatk.s18617.tournamentmanagement.repositories.GameRepository;
+import pjatk.s18617.tournamentmanagement.repositories.LocationRepository;
+import pjatk.s18617.tournamentmanagement.repositories.TournamentRepository;
 import pjatk.s18617.tournamentmanagement.repositories.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @Component
@@ -15,31 +22,96 @@ import java.util.Arrays;
 public class BootstrapData implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+    private final LocationRepository locationRepository;
+    private final TournamentRepository tournamentRepository;
 
     @Transactional
     @Override
     public void run(String... args) throws Exception {
-        loadUserData();
+        loadData();
     }
 
-    private void loadUserData() {
-        if (userRepository.count() == 0) {
+    private void loadData() {
+        if (gameRepository.count() == 0) {
 
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
             User user1 = User.builder()
                     .username("testUser")
                     .password(encoder.encode("testPassword"))
                     .description("Test description.")
                     .build();
-
             User user2 = User.builder()
                     .username("testUser2")
                     .password(encoder.encode("testPassword2"))
                     .description("Test description 2.")
                     .build();
+            userRepository.saveAllAndFlush(Arrays.asList(user1, user2));
 
-            userRepository.saveAll(Arrays.asList(user1, user2));
+
+            Game cs2 = Game.builder()
+                    .name("Counter Strike 2")
+                    .description("Counter Strike 2 description.")
+                    .build();
+            Game dota2 = Game.builder()
+                    .name("Dota 2")
+                    .description("Dota 2 description.")
+                    .build();
+            gameRepository.saveAllAndFlush(Arrays.asList(cs2, dota2));
+
+            Location location1 = Location.builder()
+                    .country("Polska")
+                    .postalCode("00-001")
+                    .city("Warszawa")
+                    .street("Uliczna")
+                    .houseNumber("12b")
+                    .build();
+            Location location2 = Location.builder()
+                    .country("Polska")
+                    .postalCode("80-001")
+                    .city("Gdańsk")
+                    .street("Bałtykowa")
+                    .houseNumber("21")
+                    .build();
+            locationRepository.saveAllAndFlush(Arrays.asList(location1, location2));
+
+            Tournament tournament1 = Tournament.builder()
+                    .name("Tournament 1")
+                    .description("Tournament 1 description.")
+                    .startDate(LocalDateTime.now())
+                    .endDate(LocalDateTime.now())
+                    .joinSecretCode("12345678")
+                    .manageSecretCode("87654321")
+                    .userOwner(user1)
+                    .game(cs2)
+                    .location(location1)
+                    .build();
+            Tournament tournament2 = Tournament.builder()
+                    .name("Tournament 1")
+                    .description("Tournament 1 description.")
+                    .startDate(LocalDateTime.now())
+                    .endDate(LocalDateTime.now())
+                    .joinSecretCode("12345678")
+                    .manageSecretCode("87654321")
+                    .userOwner(user2)
+                    .game(dota2)
+                    .location(location2)
+                    .build();
+            Tournament tournament3 = Tournament.builder()
+                    .name("Tournament 3")
+                    .description("Tournament 3 description.")
+                    .startDate(LocalDateTime.now())
+                    .endDate(LocalDateTime.now())
+                    .joinSecretCode("12345678")
+                    .manageSecretCode("87654321")
+                    .userOwner(user2)
+                    .game(dota2)
+                    // without location
+                    .build();
+            tournamentRepository.saveAllAndFlush(Arrays.asList(tournament1, tournament2, tournament3));
+
+
+
         }
     }
 
