f4fa4b7a45fc2673f5a34981f7a3a995e35c207e
shindu-aw <adamwoy1999@gmail.com>
2025-07-12 21:02:39 +0200

    feat: add LinkRepository

---
+++ src/main/java/pjatk/s18617/tournamentmanagement/repositories/LinkRepository.java
@@ -0,0 +1,7 @@
+package pjatk.s18617.tournamentmanagement.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pjatk.s18617.tournamentmanagement.model.Link;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+}
