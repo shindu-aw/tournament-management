958462556bedac26c1e407795585d6a3c7f6e797
shindu-aw <adamwoy1999@gmail.com>
2025-07-16 19:10:04 +0200

    feat: add JpaSpecificationExecutor to TournamentRepository

--- src/main/java/pjatk/s18617/tournamentmanagement/repositories/TournamentRepository.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/repositories/TournamentRepository.java
@@ -2,13 +2,14 @@ package pjatk.s18617.tournamentmanagement.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import pjatk.s18617.tournamentmanagement.model.Game;
 import pjatk.s18617.tournamentmanagement.model.Tournament;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+public interface TournamentRepository extends JpaRepository<Tournament, Long>, JpaSpecificationExecutor<Tournament> {
     List<Tournament> findByGame(Game game);
 
     List<Tournament> findByGame_Id(Long id);
