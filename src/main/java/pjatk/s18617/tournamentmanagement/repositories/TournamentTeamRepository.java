4e0ecbea310c7270f3e56f276f985fad2e33e6e8
shindu-aw <adamwoy1999@gmail.com>
2025-07-21 19:42:28 +0200

    chore: add comment

--- src/main/java/pjatk/s18617/tournamentmanagement/repositories/TournamentTeamRepository.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/repositories/TournamentTeamRepository.java
@@ -6,7 +6,7 @@ import pjatk.s18617.tournamentmanagement.model.TournamentTeam;
 
 public interface TournamentTeamRepository extends JpaRepository<TournamentTeam, Long> {
 
-    @Procedure(value = "recount_tournament_team_scores")
+    @Procedure(value = "recount_tournament_team_scores") // migration V5__add_recount_scores_procedure.sql
     void recountTournamentTeamScores(Long id);
 
 }
