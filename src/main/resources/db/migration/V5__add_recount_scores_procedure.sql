76a905d35a018455f58e988334c4788f050bf0ee
shindu-aw <adamwoy1999@gmail.com>
2025-07-23 20:17:52 +0200

    fix: recount_tournament_team_scores procedure

--- src/main/resources/db/migration/V5__add_recount_scores_procedure.sql
+++ src/main/resources/db/migration/V5__add_recount_scores_procedure.sql
@@ -1,23 +1,21 @@
 DELIMITER //
-//
+
 CREATE PROCEDURE recount_tournament_team_scores(IN tournament_id_in BIGINT)
 BEGIN
-    -- reset score_sum for all tournaments belonging to the specified tournament
+    -- reset score_sum for all tournament_teams belonging to the specified tournament
     UPDATE tournament_team
     SET score_sum = 0
     WHERE tournament_id = tournament_id_in;
 
-    -- recalculate score_sum for team_1 scores
+    -- recalculate score_sum by summing all team_1_scores and team_2_scores for each tournament_team
     UPDATE tournament_team tt
-        JOIN match_entry me ON tt.id = me.tournament_team_1_id
-    SET tt.score_sum = tt.score_sum + me.team_1_score
-    WHERE me.tournament_id = tournament_id_in;
+    SET tt.score_sum = (SELECT IFNULL(SUM(IF(me.tournament_team_1_id = tt.id, me.team_1_score, 0)), 0) +
+                               IFNULL(SUM(IF(me.tournament_team_2_id = tt.id, me.team_2_score, 0)), 0)
+                        FROM match_entry me
+                        WHERE (tt.id = me.tournament_team_1_id OR tt.id = me.tournament_team_2_id)
+                          AND me.tournament_id = tournament_id_in)
+    WHERE tt.tournament_id = tournament_id_in;
 
-    -- recalculate score_sum for team_2 scores
-    UPDATE tournament_team tt
-        JOIN match_entry me ON tt.id = me.tournament_team_2_id
-    SET tt.score_sum = tt.score_sum + me.team_2_score
-    WHERE me.tournament_id = tournament_id_in;
-END//
+END //
+
 DELIMITER ;
-
