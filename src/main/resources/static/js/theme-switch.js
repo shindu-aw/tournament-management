8024b2f108f55fb538b682e00450ba9ddd9ee417
shindu-aw <adamwoy1999@gmail.com>
2025-07-24 05:19:13 +0200

    feat: add dark-light mode theme switcher

---
+++ src/main/resources/static/js/theme-switch.js
@@ -0,0 +1,24 @@
+/*
+Source:
+https://github.com/404GamerNotFound/bootstrap-5.3-dark-mode-light-mode-switch
+ */
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('darkModeSwitch');
+
+    // Set the default theme to dark if no setting is found in local storage
+    const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'dark';
+
+    switchElement.addEventListener('change', function () {
+        if (this.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+        }
+    });
+});
