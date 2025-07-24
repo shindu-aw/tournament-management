84de4a23a836b5ccb68975009baa80258681bbc4
shindu-aw <adamwoy1999@gmail.com>
2025-07-24 05:13:17 +0200

    feat: add js and css directories to request matchers

--- src/main/java/pjatk/s18617/tournamentmanagement/config/WebSecurityConfig.java
+++ src/main/java/pjatk/s18617/tournamentmanagement/config/WebSecurityConfig.java
@@ -40,6 +40,8 @@ public class WebSecurityConfig {
                         .requestMatchers(
                                 "/webjars/**",
                                 "/images/**",
+                                "/js/**",
+                                "/css/**",
                                 "/login**",
                                 "/error",
                                 "/",
